b14ef8f1d1cd259ce474134d25c7c5c5230170a8
robuntu <robuntu@stro.ycdsb.ca>
2018-11-05 11:01:43 -0500

    Set up basic functions

--- statTools.py
+++ statTools.py
@@ -1,3 +1,35 @@
-import pytest
-from startTools import *
+# Measures of Central Tendency
+
+# the average value of all the data in the set
+def mean(numList):
+
+
+#  the middle value in a data set that has been arranged in numerical order
+def median(numList):
+   pass
+
+# is the value that occurs most frequently in the set
+def mode(numList):
+   pass
+
+# Measures of Spread
+
+# The exclusive range is the difference between the largest and smallest results in a data set
+def range(numList):
+   pass
+
+#  the median of the lower half of the data set.
+def lowerQuart(numList):
+   pass
+
+#  the median of the upper half of the data set.
+def upperQuart(numList):
+   pass
+
+def variance(numList):
+   pass
+
+def stanDev(numList):
+   pass
+
 
