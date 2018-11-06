7d0447b1b03ac09d318c64d1836000580c4dd808
Grace Leung <grace.leung19@ycdsbk12.ca>
2018-11-06 11:08:45 -0500

    Add to code of the mean, mode and lowerQuart functions

--- statTools.py
+++ statTools.py
@@ -1,3 +1,5 @@
+# Video must show red-green-blue parts, git commits are in present tense, fill out the coloumn
+
 import math
 
 # Measures of Central Tendency
@@ -9,33 +11,22 @@ def mean(numList):
        sum += numList[i]
     return sum / len(numList)
 
-#  the middle value in a data set that has been arranged in numerical order
-def median(numList):
-   pass
-
 # is the value that occurs most frequently in the set
 def mode(numList):
    most = 0
-   for i in range(len(numList)):
-       if numList.count(numList[i]) > most:
-           most = numList.count(numList[i])
+   for i in range(len([numList])):
+        if numList.count(numList[i]) > most:
+            most = numList[i]
+   return most
 
 # Measures of Spread
 
-# The exclusive range is the difference between the largest and smallest results in a data set
-def range(numList):
-   pass
-
 #  the median of the lower half of the data set.
 def lowerQuart(numList):
-   pass
-
-#  the median of the upper half of the data set.
-def upperQuart(numList):
-   pass
+   mid = len(numList) // 2
+   newList = numList[:mid]
+   print(newList)
 
 def variance(numList):
    pass
 
-def stanDev(numList):
-   pass
