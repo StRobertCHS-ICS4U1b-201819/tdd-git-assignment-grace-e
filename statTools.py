f4cc772c69fa1d318404ea160c2ad0612a042bd1
robuntu <robuntu@stro.ycdsb.ca>
2018-11-06 10:34:07 -0500

    pass test3 for median

--- statTools.py
+++ statTools.py
@@ -9,7 +9,11 @@ def mean(numList):
 def median(numList):
    numList.sort()
    middle = len(numList) // 2
-   med = numList[middle]
+   if(len(numList) % 2 == 0):
+      return (numList[middle] + numList[middle - 1]) / 2
+
+   else:
+      med = numList[middle]
    return med
 
 # is the value that occurs most frequently in the set
--- test_statTools.py
+++ test_statTools.py
@@ -6,3 +6,8 @@ def test_median_basic1():
 
 def test_median_basic2():
    assert(median([2, 4, 0, 6, 1]) == 2)
+
+def test_median_basic3():
+   assert(median([2, 4, 1, 6]) == 3)
+
+
