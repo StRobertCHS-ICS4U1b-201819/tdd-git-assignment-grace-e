69ae6a3240ac8f1ec12d94520b1fd9ba77e71e54
Your Name <you@example.com>
2018-11-11 01:39:20 -0500

    Adding Eryka's code to my repository

--- statTools.py
+++ statTools.py
@@ -85,4 +85,63 @@ def upperQuart(numList):
 def stanDev(numList):
    pass
 
+# Video must show red-green-blue parts, git commits are in present tense, fill out the coluumn, want some tests to fail
+
+# Measures of Central Tendency
+
+# the average value of all the data in the set
+def mean(numList):
+    try:
+        sum = 0
+        for i in range(len(numList)):
+           sum += numList[i]
+        return sum / len(numList)
+    except TypeError:
+        return "Must be list of numbers"
+    except NameError:
+        return "Variables have not been initialized"
+    except:
+        return "An error has occurred"
+
+
+def median(numList):
+    '''
+    Find the median of a given list
+    :param numList: list of ints
+    :return: float median of list
+    '''
+
+    numList.sort()
+    middle = len(numList) // 2
+
+    if (len(numList) % 2 == 0):
+        return (numList[middle] + numList[middle - 1]) / 2
+    else:
+        return numList[middle]
+
+# is the value that occurs most frequently in the set
+def mode(numList):
+   most = 0
+   for i in range(len([numList])):
+        if numList.count(numList[i]) > most:
+            most = numList[i]
+   return most
+
+# Measures of Spread
+
+#  the median of the lower half of the data set.
+def lowerQuart(numList):
+   numList.sort()
+   mid = round(round((len(numList) / 2)) / 2)
+   return numList[mid]
+
+
+def variance(numList):
+    acc2 = 0
+    numMean = mean(numList)
+    for i in range(len(numList)):
+        acc2 += (numList[i] - numMean) ** 2
+    x = acc2 / len(numList)
+    rounded = "{0:0.3f}".format(x)
+    return float(rounded)
 
--- test_statTools.py
+++ test_statTools.py
@@ -80,6 +80,48 @@ def test_upperQuart_input():
    with pytest.raises(TypeError) as inputerrmsg: upperQuart(["a", "b", "c"])
    assert ("Please provide numbers only" == str(inputerrmsg.value))
 
+def test_mean1():
+    assert(mean([1, 1, 1, 1, 1]) == 1)
+
+
+def test_mean2():
+    assert(mean([5, 5, 4, 54, 3, 1, 10, 7, 9, 2]) == 10)
+
+
+def test_mean_emptyList():
+    assert(mean([]) == "An error has occurred")
+
+
+def test_mean_letters():
+    assert(mean(['h', 'e', 'l', 'l', 'o']) == "Must be list of numbers")
+
+
+def test_mean_variable():
+    with pytest.raises(NameError):
+        assert(mean([e, r, i, n]) == "Variables have not been initialized")
+
+
+def test_mode1():
+    assert(mode([8, 9, 6, 0, 4, 7, 6, 8, 8, 8, 1, 3, 3]) == 8)
+
+
+def test_mode2():
+    assert(mode([9, 8, 9, 9, 7, 9, 6, 9, 5, 0, 3, 9, 6, 8]) == 9)
+
+
+def test_lowerQuart1():
+    assert(lowerQuart([1, 2, 3, 4, 4, 5, 20, 30, 40, 43]) == 3)
+
+
+def test_lowerQuart2():
+    assert(lowerQuart([1,65, 67, 20, 4, 5, 68, 30, 40, 55, 43, 4, 2]) == 4)
+
+
+def test_variance1():
+    assert(variance([3, 4, 5, 5, 6, 7, 9]) == 3.388)
+
+def test_variance2():
+    assert(variance([0,4,4,7,7,7,8,8,8,9,9,11,11,11]) == 8.816)
 
 
 
