09eaa10f26f22da09dcb2dc95625e27db0935c7e
shishune <H3yMyGuy;)>
2018-11-10 22:30:02 -0500

    Fail range empty test

--- test_statTools.py
+++ test_statTools.py
@@ -25,6 +25,59 @@ def test_median_basic3():
 def test_median_basic4():
    assert(median([1, 2, 3, 4]) == 2.5)
 
+def test_median_empty():
+   with pytest.raises(ValueError) as emptyerrmsg: median([])
+   assert ("Illegal empty list" == str(emptyerrmsg.value))
+
+def test_median_input():
+   with pytest.raises(TypeError) as inputerrmsg: median(["no", "hi!"])
+   assert ("Please provide numbers only" == str(inputerrmsg.value))
+
+
+
+def test_range_basic1():
+    assert(range([2, 4, 1, 5, 9]) == 8)
+
+def test_range_basic2():
+   assert(range([2, 4, 0, 6, 1]) == 6)
+
+def test_range_basic3():
+   assert(range([2, 7, -3, 6, 1]) == 10)
+
+def test_range_basic4_neg():
+   assert(range([-8, -3, -9, -2]) == 7)
+
+def test_range_empty():
+   with pytest.raises(ValueError) as emptyerrmsg: range([])
+   assert ("Illegal empty list" == str(emptyerrmsg.value))
+
+
+
+def test_upperQuart_basic1():
+    assert(upperQuart([2, 4, 1, 5, 9]) == 5)
+
+def test_upperQuart_basic2():
+   assert(upperQuart([2, 4, 0, 6, 1]) == 4)
+
+def test_upperQuart_basic3():
+   assert(upperQuart([2, 4, 1, 6, 0, 7]) == 6)
+
+def test_upperQuart_basic4():
+   assert(upperQuart([3, 4, 1, 7, 8, 6]) == 7)
+
+def test_upperQuart_basic5():
+   assert(upperQuart([2, 4, 1, 6]) == 5)
+
+def test_upperQuart_empty():
+   with pytest.raises(ValueError) as emptyerrmsg: upperQuart([])
+   assert ("Illegal empty list" == str(emptyerrmsg.value))
+
+def test_upperQuart_input():
+   with pytest.raises(TypeError) as inputerrmsg: upperQuart(["a", "b", "c"])
+   assert ("Please provide numbers only" == str(inputerrmsg.value))
+
+
+
 
 
 
